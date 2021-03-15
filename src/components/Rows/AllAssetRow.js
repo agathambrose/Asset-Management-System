a8ff269dc9ff5a22b1c39994144616e844bf5836
ijeomaemeruwa <helloijeoma@gmail.com>
2021-03-15 10:06:16 +0000

    implement view assets page ui

--- src/components/Rows/AllAssetRow.js
+++ src/components/Rows/AllAssetRow.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import EditAssetModal from "../EditAsset/EditAssetModal";
+import { Link } from "react-router-dom";
 
 const AllAssetRow = ({ sn, name, category, quantity, location, edit }) => {
   const [modal, setModal] = useState(null);
@@ -10,13 +11,11 @@ const AllAssetRow = ({ sn, name, category, quantity, location, edit }) => {
   return (
     <>
       <div className="bg-white text-lg text-center font-semibold md:h-14 p-2 mb-0.5 mx-1 flex items-center justify-center  h-16">
-        <h3
-          className="font-black mr-0.5 px-2 w-4
-           sm:w-12  "
-        >
-          {sn}
-        </h3>
-        <h4 className="text-center mr-0.5 px-2 w-3/6 ">{name}</h4>
+        <h3 className="font-black mr-0.5 px-2 w-4 sm:w-12 ">{sn}</h3>
+        <h4 className="text-center mr-0.5 px-2 w-3/6 ">
+          {/* Update this later */}
+          <Link to={{ pathname: "/view-assets" }}>{name}</Link>
+        </h4>
         <h4 className=" mr-0.5 px-2 w-1/6">{category}</h4>
         <h4 className=" mr-0.5 px-2 w-1/6">{quantity}</h4>
         <h4 className=" mr-0.5 px-2 w-2/6">{location}</h4>
